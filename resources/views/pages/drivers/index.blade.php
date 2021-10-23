996378472a70d1321f4555a48b0c51675662c22c
mixyespitlick <mascarinasarniel@gmail.com>
2021-10-23 16:53:58 +0800

    Applied flatable theme on driver component

--- resources/views/pages/drivers/index.blade.php
+++ resources/views/pages/drivers/index.blade.php
@@ -4,61 +4,100 @@
 
 @push('styles')
 <!-- Custom styles for this page -->
-<link href="{{ asset('vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+<link rel="stylesheet" type="text/css"
+    href="{{ asset('bower_components/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('pages/data-table/css/buttons.dataTables.min.css') }}">
+<link rel="stylesheet" type="text/css"
+    href="{{ asset('bower_components/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}">
 @endpush
 
 <!-- Page Heading -->
-<div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Drivers</h1>
+<div class="page-header">
+    <div class="page-header-title">
+        {{-- <h4>Driver</h4>
+        <span>List of Drivers</span> --}}
+    </div>
+    <div class="page-header-breadcrumb">
+        <ul class="breadcrumb-title">
+            <li class="breadcrumb-item">
+                <a href="index-2.html">
+                    <i class="icofont icofont-home"></i>
+                </a>
+            </li>
+            <li class="breadcrumb-item"><a href="#!">Data Table</a>
+            </li>
+            <li class="breadcrumb-item"><a href="#!">Basic Initialization</a>
+            </li>
+        </ul>
+    </div>
 </div>
 
 <!-- Content Row -->
-<div class="row">
-    <div class="col-xl-12 col-lg-12 col-md-12 col-12 m-auto">
-        @if(Session::has('success'))
-        <div class="alert alert-success alet-dismissible fade show" role="alert">
-            {{ Session::get('success') }}
-            @php
-            Session::forget('success');
-            @endphp
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        @endif
-        <div class="card mb-4 py-3 border-bottom-primary">
-            <div class="card-body">
-                <table class="table table-bordered table-hovered" id="driverTable">
-                    <thead>
-                        <th> ID </th>
-                        <th> First Name </th>
-                        <th> Last Name </th>
-                        <th> Action </th>
-                    </thead>
-                    <tbody>
-                        <?php 
-                    foreach($drivers as $driver) : ?>
-                        <tr>
-                            <td> <?php echo $driver['id']; ?> </td>
-                            <td> <?php echo $driver['fname']; ?> </td>
-                            <td> <?php echo $driver['lname']; ?> </td>
-                            <td>
+<div class="page-body">
+    <div class="row">
+        <div class="col-sm-12">
+            @if(Session::has('success'))
+            <div class="alert alert-success alet-dismissible fade show" role="alert">
+                {{ Session::get('success') }}
+                @php
+                Session::forget('success');
+                @endphp
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            @endif
+            <div class="card">
+                <div class="card-header">
+                    <h5>Drivers</h5>
+                    <span>List of Drivers</span>
+                    <div class="card-header-right">
+                        <i class="icofont icofont-rounded-down"></i>
+                        <i class="icofont icofont-refresh"></i>
+                        <i class="icofont icofont-close-circled"></i>
+                    </div>
+                </div>
+                <div class="card-block">
+                    <div class="dt-responsive table-responsive">
+                        <table class="table table-striped table-bordered nowrap" id="driverTable">
+                            <thead>
+                                <th> ID </th>
+                                <th> First Name </th>
+                                <th> Last Name </th>
+                                <th> Action </th>
+                            </thead>
+                            <tbody>
+                                <?php 
+                        foreach($drivers as $driver) : ?>
+                                <tr>
+                                    <td>
+                                        <?php echo $driver['id']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $driver['fname']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $driver['lname']; ?>
+                                    </td>
+                                    <td>
 
-                                <form action="{{ route('drivers.destroy',$driver->id) }}" method="POST">
-                                    <a href="{{ route('drivers.edit',$driver->id) }}"
-                                        class="d-none d-sm-inline-block btn btn-sm btn-warning"> <i
-                                            class="fas fa-exclamation-triangle"></i> Edit
-                                    </a>
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="d-none d-sm-inline-block btn btn-sm btn-danger" type="submit"> <i
-                                            class="fas fa-trash"></i> Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+                                        <form action="{{ route('drivers.destroy',$driver->id) }}" method="POST">
+                                            <a href="{{ route('drivers.edit',$driver->id) }}"
+                                                class="d-none d-sm-inline-block btn btn-sm btn-warning"> <i
+                                                    class="fas fa-exclamation-triangle"></i> Edit
+                                            </a>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="d-none d-sm-inline-block btn btn-sm btn-danger"
+                                                type="submit"> <i class="fas fa-trash"></i> Delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -68,8 +107,17 @@
 @push('scripts')
 <!-- CDN jQuery Datatable -->
 <!-- Page level plugins -->
-<script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('bower_components/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('bower_components/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('pages/data-table/js/jszip.min.js') }}"></script>
+<script src="{{ asset('pages/data-table/js/pdfmake.min.js') }}"></script>
+<script src="{{ asset('pages/data-table/js/vfs_fonts.js') }}"></script>
+<script src="{{ asset('bower_components/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('bower_components/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('bower_components/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('bower_components/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('bower_components/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
+{{-- <script src="{{ asset('pages/data-table/js/data-table-custom.js') }}"></script> --}}
 <script>
     $(document).ready(function() {
     	$('#driverTable').DataTable();
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,122 +1,59 @@
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-    <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
-    </a>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0">
-
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Interface
+<nav class="pcoded-navbar" pcoded-header-position="relative">
+    <div class="sidebar_toggle">
+        <a href="#"><i class="icon-close icons"></i></a>
     </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-            aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Components</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="buttons.html">Buttons</a>
-                <a class="collapse-item" href="cards.html">Cards</a>
+    <div class="pcoded-inner-navbar main-menu">
+        <div class="">
+            <div class="main-menu-header">
+                <img class="img-40" src="{{ asset('images/user.png') }}" alt="User-Profile-Image" />
+                <div class="user-details">
+                    <span>John Doe</span>
+                    <span id="more-details">UX Designer<i class="ti-angle-down"></i></span>
+                </div>
+            </div>
+            <div class="main-menu-content">
+                <ul>
+                    <li class="more-details">
+                        <a href="user-profile.html"><i class="ti-user"></i>View Profile</a>
+                        <a href="#!"><i class="ti-settings"></i>Settings</a>
+                        <a href="#!"><i class="ti-layout-sidebar-left"></i>Logout</a>
+                    </li>
+                </ul>
             </div>
         </div>
-    </li>
-
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-            aria-expanded="true" aria-controls="collapseUtilities">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Utilities</span>
-        </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Utilities:</h6>
-                <a class="collapse-item" href="utilities-color.html">Colors</a>
-                <a class="collapse-item" href="utilities-border.html">Borders</a>
-                <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                <a class="collapse-item" href="utilities-other.html">Other</a>
-            </div>
+        {{-- <div class="pcoded-navigatio-lavel" data-i18n="nav.category.navigation" menu-title-theme="theme5">
+            Navigation
+        </div> --}}
+        <ul class="pcoded-item pcoded-left-item">
+            <li class="pcoded-hasmenu active pcoded-trigger">
+                <a href="javascript:void(0)">
+                    <span class="pcoded-micon"><i class="ti-home"></i></span>
+                    <span class="pcoded-mtext" data-i18n="nav.dash.main">Dashboard</span>
+                    <span class="pcoded-mcaret"></span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class="active">
+                        <a href="index-2.html">
+                            <span class="pcoded-micon"><i class="ti-angle-right"></i></span>
+                            <span class="pcoded-mtext" data-i18n="nav.dash.default">Default</span>
+                            <span class="pcoded-mcaret"></span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <div class="pcoded-navigatio-lavel" data-i18n="nav.category.forms" menu-title-theme="theme5">
+            Management
         </div>
-    </li>
+        <ul class="pcoded-item pcoded-left-item">
+            <li class="">
+                <a href="{{ route('drivers.index') }}" data-i18n="nav.form-wizard.main">
+                    <span class="pcoded-micon"><i class="ti-archive"></i></span>
+                    <span class="pcoded-mtext">Driver</span>
+                    <span class="pcoded-mcaret"></span>
+                </a>
+            </li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Addons
+        </ul>
     </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
-            aria-controls="collapsePages">
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
-        </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Login Screens:</h6>
-                <a class="collapse-item" href="login.html">Login</a>
-                <a class="collapse-item" href="register.html">Register</a>
-                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                <div class="collapse-divider"></div>
-                <h6 class="collapse-header">Other Pages:</h6>
-                <a class="collapse-item" href="404.html">404 Page</a>
-                <a class="collapse-item" href="blank.html">Blank Page</a>
-            </div>
-        </div>
-    </li>
-
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span></a>
-    </li>
-
-    <!-- Nav Item - Tables -->
-    <li class="nav-item">
-        <a class="nav-link" href="tables.html">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Tables</span></a>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
-
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
-
-    <!-- Sidebar Message -->
-    <div class="sidebar-card d-none d-lg-flex">
-        <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
-        <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!
-        </p>
-        <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
-    </div>
-
-</ul>
+</nav>
