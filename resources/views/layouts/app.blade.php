2d22ba25b4488b62390e49159f727711c1258fe6
mixyespitlick <mascarinasarniel@gmail.com>
2021-10-24 10:49:14 +0800

    Updated theme for login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,46 +1,111 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<!-- Mirrored from flatable.phoenixcoded.net/default/auth-normal-sign-in.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 10 Jan 2019 12:15:52 GMT -->
+
 <head>
+    <title>Flat Able - Premium Admin Template by Phoenixcoded</title>
+
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
 
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="Phoenixcoded">
+    <meta name="keywords"
+        content=", Flat ui, Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
+    <meta name="author" content="Phoenixcoded">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
 
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
 
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
 
+    <link rel="stylesheet" type="text/css" href="{{ asset('icon/themify-icons/themify-icons.css') }}">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('icon/icofont/css/icofont.css') }}">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/color/color-1.css') }}" id="color" />
 </head>
 
-<body id="page-top" class="bg-gradient-primary">
+<body class="fix-menu">
     @yield('content')
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!--[if lt IE 9]>
+<div class="ie-warning">
+    <h1>Warning!!</h1>
+    <p>You are using an outdated version of Internet Explorer, please upgrade <br/>to any of the following web browsers to access this website.</p>
+    <div class="iew-container">
+        <ul class="iew-download">
+            <li>
+                <a href="http://www.google.com/chrome/">
+                    <img src="assets/images/browser/chrome.png" alt="Chrome">
+                    <div>Chrome</div>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.mozilla.org/en-US/firefox/new/">
+                    <img src="assets/images/browser/firefox.png" alt="Firefox">
+                    <div>Firefox</div>
+                </a>
+            </li>
+            <li>
+                <a href="http://www.opera.com">
+                    <img src="assets/images/browser/opera.png" alt="Opera">
+                    <div>Opera</div>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.apple.com/safari/">
+                    <img src="assets/images/browser/safari.png" alt="Safari">
+                    <div>Safari</div>
+                </a>
+            </li>
+            <li>
+                <a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie">
+                    <img src="assets/images/browser/ie.png" alt="">
+                    <div>IE (9 & above)</div>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <p>Sorry for the inconvenience!</p>
+</div>
+<![endif]-->
 
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
 
-    <!-- Page level plugins -->
-    <script src="vendor/chart.js/Chart.min.js"></script>
+    <script type="text/javascript" src="{{ asset('bower_components/jquery/dist/jquery.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('bower_components/jquery-ui/jquery-ui.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('bower_components/tether/dist/js/tether.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 
-    <!-- Page level custom scripts -->
-    <script src="js/demo/chart-area-demo.js"></script>
-    <script src="js/demo/chart-pie-demo.js"></script>
+    <script type="text/javascript" src="{{ asset('bower_components/jquery-slimscroll/jquery.slimscroll.js') }}">
+    </script>
 
+    <script type="text/javascript" src="{{ asset('bower_components/modernizr/modernizr.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('bower_components/modernizr/feature-detects/css-scrollbars.js') }}">
+    </script>
+
+    <script type="text/javascript" src="{{ asset('bower_components/i18next/i18next.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('bower_components/i18next-xhr-backend/i18nextXHRBackend.min.js') }}">
+    </script>
+    <script type="text/javascript"
+        src="{{ asset('bower_components/i18next-browser-languagedetector/i18nextBrowserLanguageDetector.min.js') }}">
+    </script>
+    <script type="text/javascript" src="{{ asset('bower_components/jquery-i18next/jquery-i18next.min.js') }}"></script>
+
+    <script type="text/javascript" src="{{ asset('js/script.js') }}"></script>
+
+    <script type="text/javascript" src="{{ asset('js/common-pages.js') }}"></script>
 </body>
+
+<!-- Mirrored from flatable.phoenixcoded.net/default/auth-normal-sign-in.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 10 Jan 2019 12:15:56 GMT -->
 
 </html>
--- resources/views/pages/drivers/edit.blade.php
+++ resources/views/pages/drivers/edit.blade.php
@@ -2,42 +2,76 @@
 
 @section('content')
 <!-- Page Heading -->
-<div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Edit Driver</h1>
-    <a href="{{ route('drivers.index') }}" class="d-none d-sm-inline-block btn btn-sm btn-danger shadow-sm"><i
-            class="fas fa-backward fa-sm text-white-50"></i> Back</a>
+<div class="page-header">
+    <div class="page-header-title">
+        <h4>Update Driver</h4>
+        {{-- <span>List of Drivers</span> --}}
+    </div>
+    <div class="page-header-breadcrumb">
+        <ul class="breadcrumb-title">
+            <li class="breadcrumb-item">
+                <a href="index-2.html">
+                    <i class="icofont icofont-home"></i>
+                </a>
+            </li>
+            <li class="breadcrumb-item"><a href="#!">Data Table</a>
+            </li>
+            <li class="breadcrumb-item"><a href="#!">Basic Initialization</a>
+            </li>
+        </ul>
+    </div>
 </div>
 
 <!-- Content Row -->
-<div class="row">
-    <div class="col-lg-6">
-        @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
-        <div class="card mb-4 py-3 border-bottom-primary">
-            <div class="card-body">
-                <form action="{{ route('drivers.update',$driver->id) }}" method="POST">
-                    @csrf
-                    @method('PUT')
-                    <div class="for-group mb-3">
-                        <label>First Name</label>
-                        <input type="text" name="fname" value="{{ $driver->fname }}" class="form-control">
-                        {{-- {!! $errors->first('first_name', '<small class="text-danger">:message</small>') !!} --}}
+<div class="page-body">
+    <div class="row">
+        <div class="col-sm-6">
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+
+            <div class="card">
+                <div class="card-header">
+                    <h5>UPDATE DRIVER INFORMATION</h5>
+                    {{-- <span>Add class of <code>.form-control</code> with <code>&lt;input&gt;</code> tag</span> --}}
+                    <div class="card-header-right">
+                        <i class="icofont icofont-rounded-down"></i>
+                        <i class="icofont icofont-refresh"></i>
+                        <i class="icofont icofont-close-circled"></i>
                     </div>
-                    <div class="for-group mb-3">
-                        <label>Last Name</label>
-                        <input type="text" name="lname" value="{{ $driver->lname }}" class="form-control">
-                    </div>
-                    <div class="for-group mb-3">
-                        <button type="submit" class="btn btn-sm btn-primary">Save</button>
-                    </div>
-                </form>
+                </div>
+                <div class="card-block">
+                    <form action="{{ route('drivers.update',$driver->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">First Name</label>
+                            <div class="col-sm-10">
+                                <input class="form-control" type="text" name="fname" value="{{ $driver->fname }}">
+                            </div>
+                            {{-- {!! $errors->first('first_name', '<small class="text-danger">:message</small>') !!}
+                            --}}
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Last Name</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="lname" value="{{ $driver->lname }}" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2"></label>
+                            <div class="col-sm-10">
+                                <button type="submit" class="btn btn-primary m-b-0">Save</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
